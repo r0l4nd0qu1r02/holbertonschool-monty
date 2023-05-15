ead1cc983d616eb2df7517e236569ed98f42cbd6
r0l4nd0qu1r02 <r0l4nd0qu1r02@gmail.com>
2023-05-15 16:38:15 -0500

    Monty project

---
+++ functions.c
@@ -0,0 +1,70 @@
+#include "monty.h"
+
+/**
+ * push - Adds a new node at the beginning of a stack_t list.
+ * @stack: Double pointer to the first node of the list.
+ * @line_number: Data value for the new node.
+ *
+ * Return: NULL on failure or address of the new element on success.
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+    char *number = NULL;
+    int n;
+
+    number = strtok(NULL, " \n");
+    if (number == NULL)
+    {
+        dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    if (strcmp(number, "0") == 0)
+        n = 0;
+    else
+    {
+        n = atoi(number);
+        if (n == 0)
+        {
+            dprintf(STDERR_FILENO, "L%u: usage: push integer\n", line_number);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    printf("Numero fuera de la pila %d\n", n);
+    
+    stack_t *new_node = NULL;
+
+	if (stack == NULL)
+		return;
+
+	new_node = (stack_t *)malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		return;
+
+	new_node->n = n;
+	new_node->prev = NULL;
+
+	if (*stack == NULL)
+		new_node->next = NULL;
+
+	if (*stack)
+	{
+		new_node->next = *stack;
+		(*stack)->prev = new_node;
+	}
+
+	*stack = new_node;
+
+
+    printf("Numero dentro de la pila %d\n", (*stack)->n);
+
+	return;
+}
+
+
+
+
+
+
+
---
+++ monty.c
@@ -0,0 +1,60 @@
+#include "monty.h"
+
+/**
+ * main - Executes de commands of a monty file
+ * @argc: Size of argv
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char **argv)
+{
+    FILE *file;
+
+    char *line = NULL;
+    size_t lenght_of_line = 0;
+    ssize_t number_of_characters_read = 0;
+
+    unsigned int line_number = 1;
+    char *command = NULL;
+
+    stack_t *stack = NULL;
+    
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n"); 
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1],"r");
+
+    if (file == NULL)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]); 
+        exit(EXIT_FAILURE); 
+    }
+
+    number_of_characters_read = getline(&line, &lenght_of_line, file);
+
+    if (number_of_characters_read == -1)
+        printf("This file is empty\n");
+    
+    if (line_number == 1)
+        fclose(file);
+
+    /* printf("%s\n", line); */
+
+    command = strtok(line, " \n");
+    /*
+        if (command == NULL)
+
+    */
+ 
+    if (strcmp(command, "push") == 0)
+    {
+        push(&stack, line_number);
+    }
+
+    return (EXIT_SUCCESS);
+}
